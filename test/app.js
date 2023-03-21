5783169a3eac73d2d67c71a4c1b901cfff143881
TinovH <9019926@student.zadkine.nl>
2023-03-21 09:49:55 +0100

    javascr begin + test

--- test/app.js
+++ test/app.js
@@ -5,8 +5,9 @@ function showAnswers() {
     const ont2 = document.getElementById("ont2").value;
     const heen = document.getElementById("heen").value;
     const terug = document.getElementById("terug").value;
-
+  
     const answers = document.getElementById("answers");
     answers.innerHTML = `<p>p1: ${p1}</p><p>p2: ${p2}</p><p>ont1: ${ont1}</p><p>ont2: ${ont2}</p><p>heen: ${heen}</p><p>terug: ${terug}</p>`;
     answers.style.display = "block";
-}
+  }
+  
